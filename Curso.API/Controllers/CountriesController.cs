b2b6944fa85aaba38a8f971b93316d83015c25b2
DevsNetDesign <developers@netdesign-ec.net>
2025-02-28 15:34:43 -0500

    crear el primer controlador

---
+++ Curso.API/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using Curso.API.Data;
+using Curso.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContex _contex;
+
+        public CountriesController(DataContex contex)
+        {
+            _contex = contex;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCountryAsync()
+        {
+            return Ok(await _contex.Conuntries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostCountryAsync(Country country)
+        {
+            _contex.Add(country);
+            await _contex.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
